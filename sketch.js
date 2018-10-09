fce68cb535823ab50e18beb5bd325ce29b8192c1
lorenzodrago <43404051+lorenzodrago@users.noreply.github.com>
2018-10-09 15:16:17 +0200

    did the thing

--- sketch.js
+++ sketch.js
@@ -1,11 +1,37 @@
-function preload(){
-  // put preload code here
+function preload() {
+    // put preload code here
 }
 
 function setup() {
-  // put setup code here
+    colorMode(HSB);
+    noStroke()
+    createCanvas(800, 800);
+    rectMode(CENTER);
+
 }
 
+
+
+var x, y;
+var radius = 15;
+var lerpVar = 0;
+
 function draw() {
-  // put drawing code here
+  /*if (mouseX>250) {
+    radius = 30;
+  } else {
+    radius = 15;
+  }*/
+  background(200, 30, 20);
+    for (x = radius; x < width-radius; x += radius) {
+        for (y = radius; y < (height-radius); y += radius) {
+            lerpVar = ((frameCount * 1 / width * x / 100) * (frameCount * 1 / height * y / 100)) % 1;
+            if (lerpVar > 0.5) {
+                lerpVar = 1 - lerpVar;
+            }
+            fill(lerpColor(color(0,70,100), color(255,70,100), lerpVar*2));
+            rect(cos(lerpVar*20)*3+x,sin(lerpVar*20)*3+y, radius*0.2, radius*0.8);
+            //ellipse(cos(lerpVar*20)*3+x,sin(lerpVar*20)*3+y, radius*0.7)
+        }
+    }
 }
